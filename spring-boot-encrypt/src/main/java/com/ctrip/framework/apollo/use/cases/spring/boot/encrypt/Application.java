9df14c45662e19877703a2325b7945615cba8143
nobodyiam <nobodyiam@gmail.com>
2019-05-01 16:36:31 +0800

    update encrypt demo

--- spring-boot-encrypt/src/main/java/com/ctrip/framework/apollo/use/cases/spring/boot/encrypt/Application.java
+++ spring-boot-encrypt/src/main/java/com/ctrip/framework/apollo/use/cases/spring/boot/encrypt/Application.java
@@ -1,12 +1,15 @@
 package com.ctrip.framework.apollo.use.cases.spring.boot.encrypt;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by kl on 2018/6/25.
@@ -14,7 +17,7 @@ import javax.annotation.PostConstruct;
  */
 @SpringBootApplication
 @EnableApolloConfig
-public class Application implements CommandLineRunner {
+public class Application implements CommandLineRunner, EnvironmentAware {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -26,9 +29,20 @@ public class Application implements CommandLineRunner {
     @Value("${test.input1}")
     private String input1;
 
+    private Environment environment;
+
     @Override
     public void run(String... args) throws Exception {
-        System.err.println("test.input 值 ENC(Ore69lUopDHL5R8Bw/G3bQ==) 解密后:" + input);
-        System.err.println("test.input1 不需要解密:" + input1);
+        while (true) {
+            System.err.println("test.input 值 ENC(Ore69lUopDHL5R8Bw/G3bQ==) 解密后:" + input);
+            System.err.println("test.input from environment: " + environment.getProperty("test.input"));
+            System.err.println("test.input1 不需要解密:" + input1);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
