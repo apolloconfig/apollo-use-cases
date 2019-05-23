f9af063a1715e8a4c594e3b254fb3555c6941352
ksewen <261517946@qq.com>
2019-05-23 14:40:07 +0800

    update preDestroyGatewayProperties logic and make it better

--- spring-cloud-gateway/src/main/java/com/ctrip/framework/apollo/use/cases/spring/cloud/gateway/GatewayPropertiesRefresher.java
+++ spring-cloud-gateway/src/main/java/com/ctrip/framework/apollo/use/cases/spring/cloud/gateway/GatewayPropertiesRefresher.java
@@ -1,5 +1,7 @@
 package com.ctrip.framework.apollo.use.cases.spring.cloud.gateway;
 
+import com.ctrip.framework.apollo.enums.PropertyChangeType;
+import com.ctrip.framework.apollo.model.ConfigChange;
 import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
 import org.slf4j.Logger;
@@ -15,6 +17,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * @author ksewen
@@ -24,6 +27,10 @@ import org.springframework.stereotype.Component;
 public class GatewayPropertiesRefresher implements ApplicationContextAware,ApplicationEventPublisherAware {
 
     private static final Logger logger = LoggerFactory.getLogger(GatewayPropertiesRefresher.class);
+
+    private static final String ID_PATTERN = "spring\\.cloud\\.gateway\\.routes\\[\\d+\\]\\.id";
+
+    private static final String DEFAULT_FILTER_PATTERN = "spring\\.cloud\\.gateway\\.default-filters\\[\\d+\\]\\.name";
 
     private ApplicationContext applicationContext;
 
@@ -61,7 +68,7 @@ public class GatewayPropertiesRefresher implements ApplicationContextAware,Appli
      */
     private void refreshGatewayProperties(ConfigChangeEvent changeEvent) {
         logger.info("Refreshing GatewayProperties!");
-        preDestroyGatewayProperties();
+        preDestroyGatewayProperties(changeEvent);
         this.applicationContext.publishEvent(new EnvironmentChangeEvent(changeEvent.changedKeys()));
         refreshGatewayRouteDefinition();
         logger.info("GatewayProperties refreshed!");
@@ -71,17 +78,25 @@ public class GatewayPropertiesRefresher implements ApplicationContextAware,Appli
      * GatewayProperties没有@PreDestroy和destroy方法
      * org.springframework.cloud.context.properties.ConfigurationPropertiesRebinder#rebind(java.lang.String)中destroyBean时不会销毁当前对象
      * 如果把spring.cloud.gateway.前缀的配置项全部删除（例如需要动态删除最后一个路由的场景），initializeBean时也无法创建新的bean，则return当前bean
-     * 这个方法提供了类似@PreDestroy的操作，把org.springframework.cloud.gateway.config.GatewayProperties#routes和org.springframework.cloud.gateway.config.GatewayProperties#defaultFilters两个集合清空
+     * 若仍保留有spring.cloud.gateway.routes[n]或spring.cloud.gateway.default-filters[n]等配置，initializeBean时会注入新的属性替换已有的bean
+     * 这个方法提供了类似@PreDestroy的操作，根据配置文件的实际情况把org.springframework.cloud.gateway.config.GatewayProperties#routes
+     * 和org.springframework.cloud.gateway.config.GatewayProperties#defaultFilters两个集合清空
      *
      * @param
      * @return void
      * @author ksewen
      * @date 2019/5/21 2:13 PM
      */
-    private synchronized void preDestroyGatewayProperties() {
+    private synchronized void preDestroyGatewayProperties(ConfigChangeEvent changeEvent) {
         logger.info("Pre Destroy GatewayProperties!");
-        this.gatewayProperties.getRoutes().clear();
-        this.gatewayProperties.getDefaultFilters().clear();
+        final boolean needClearRoutes = this.checkNeedClear(changeEvent, ID_PATTERN);
+        if (needClearRoutes) {
+            this.gatewayProperties.getRoutes().clear();
+        }
+        final boolean needClearDefaultFilters = this.checkNeedClear(changeEvent, DEFAULT_FILTER_PATTERN);
+        if (needClearDefaultFilters) {
+            this.gatewayProperties.getDefaultFilters().clear();
+        }
         logger.info("Pre Destroy GatewayProperties finished!");
     }
 
@@ -89,6 +104,24 @@ public class GatewayPropertiesRefresher implements ApplicationContextAware,Appli
         logger.info("Refreshing Gateway RouteDefinition!");
         this.publisher.publishEvent(new RefreshRoutesEvent(this));
         logger.info("Gateway RouteDefinition refreshed!");
+    }
+
+    /***
+     * 根据changeEvent和定义的pattern匹配key，如果所有对应PropertyChangeType为DELETED则需要清空GatewayProperties里相关集合
+     *
+     * @param changeEvent
+     * @param pattern
+     * @return boolean
+     * @author ksewen
+     * @date 2019/5/23 2:18 PM
+     */
+    private boolean checkNeedClear(ConfigChangeEvent changeEvent, String pattern) {
+
+        return changeEvent.changedKeys().stream().filter(key -> key.matches(pattern))
+                .filter(key -> {
+                    ConfigChange change = changeEvent.getChange(key);
+                    return PropertyChangeType.DELETED.equals(change.getChangeType());
+                }).count() > 0;
     }
 
 }
