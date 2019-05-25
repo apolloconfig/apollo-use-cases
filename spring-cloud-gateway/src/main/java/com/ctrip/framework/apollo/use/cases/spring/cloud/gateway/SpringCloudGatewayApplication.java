84d80ebbb327b26ea9bdb16aecc6f28bb5417082
ksewen <261517946@qq.com>
2019-05-25 20:34:01 +0800

    add spring cloud gateway demo (#26)

---
+++ spring-cloud-gateway/src/main/java/com/ctrip/framework/apollo/use/cases/spring/cloud/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ctrip.framework.apollo.use.cases.spring.cloud.gateway;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableApolloConfig
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+}
