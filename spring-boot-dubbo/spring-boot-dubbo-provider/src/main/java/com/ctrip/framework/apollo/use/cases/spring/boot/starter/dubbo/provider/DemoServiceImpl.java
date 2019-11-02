08b49559da7be0ea8f269c6c40c7a2e38f295025
nobodyiam <nobodyiam@gmail.com>
2019-11-02 18:34:41 +0800

    add logback-spring.xml sample

--- spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/ctrip/framework/apollo/use/cases/spring/boot/starter/dubbo/provider/DemoServiceImpl.java
+++ spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/ctrip/framework/apollo/use/cases/spring/boot/starter/dubbo/provider/DemoServiceImpl.java
@@ -2,12 +2,16 @@ package com.ctrip.framework.apollo.use.cases.spring.boot.starter.dubbo.provider;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.ctrip.framework.apollo.use.cases.spring.boot.starter.dubbo.api.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class DemoServiceImpl implements DemoService {
+  private static final Logger LOGGER = LoggerFactory.getLogger(DemoServiceImpl.class);
 
   @Override
   public String sayHello(String name) {
+    LOGGER.info("Say hello to {}", name);
     return "Hello " + name;
   }
 }
